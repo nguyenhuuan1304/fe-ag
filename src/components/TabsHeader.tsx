fa2854c807e95e6ddad1f1018dd86db74bcf8717
nhuuan <nguyenhuuan1304@gmail.com>
2025-07-21 16:46:21 +0700

    refactor: Simplify TabsHeader component by removing redundant role checks for tab visibility

--- src/components/TabsHeader.tsx
+++ src/components/TabsHeader.tsx
@@ -24,16 +24,11 @@ export function TabsHeader() {
     <div className="flex w-full flex-col gap-6">
       <Tabs value={value} onValueChange={setValue} className="gap-6">
         <TabsList className="flex gap-2">
-          {["ADMIN", "GDV_TTQT", "KSV_TTQT"].includes(user?.role) && (
-            <TabsTrigger value="unsubmitted">Báo cáo chưa bổ sung</TabsTrigger>
-          )}
+          <TabsTrigger value="unsubmitted">Báo cáo chưa bổ sung</TabsTrigger>
           <TabsTrigger value="submitted">Đã bổ sung</TabsTrigger>
-          {["ADMIN", "GDV_TTQT", "KSV_TTQT"].includes(user?.role) && (
-            <TabsTrigger value="overdue">Báo cáo quá hạn</TabsTrigger>
-          )}
+          <TabsTrigger value="overdue">Báo cáo quá hạn</TabsTrigger>
           {["ADMIN", "GDV_TTQT"].includes(user?.role) && (
             <>
-              <TabsTrigger value="overdue">Báo cáo quá hạn</TabsTrigger>
               <TabsTrigger value="notSendMailList">
                 Danh sách chưa gửi email
               </TabsTrigger>
